db5bbea9d353bd57364cd56be9b91da87d3d0122
Ubuntu <ubuntu@ip-172-31-39-229.eu-north-1.compute.internal>
2026-08-21 04:14:18 +0000

    updated application to v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Jenkins CI/CD Project is Running!"
+    return "Jenkins CI/CD Project v2 is Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
